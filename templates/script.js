6be3928d21d9d9de7a94cf0c9b744b4f8ae3422e
kaylepam <yeti01@naver.com>
2023-08-12 00:46:18 +0900

    3day

--- templates/script.js
+++ templates/script.js
@@ -1,4 +1,17 @@
-function smoothScroll(targetId) {
-    const target = document.querySelector(targetId);
-    target.scrollIntoView({ behavior: "smooth" });
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.getElementById('navbar');
+    const studyingOffsetTop = document.getElementById('studying').offsetTop;
+
+    document.querySelectorAll('nav button').forEach(button => {
+        button.addEventListener('click', function() {
+            const targetId = this.getAttribute('data-scroll-target');
+            const target = document.querySelector(targetId);
+            target.scrollIntoView({ behavior: "smooth" });
+        });
+    });
+
+    window.addEventListener('scroll', function() {
+        const showNavbar = window.scrollY >= studyingOffsetTop;
+        navbar.style.display = showNavbar ? 'flex' : 'none';
+    });
+});
