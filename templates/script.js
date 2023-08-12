000c3a27527dd150cc69ff18011c88e26324b04c
kaylepam <yeti01@naver.com>
2023-08-12 12:24:01 +0900

    4day

--- templates/script.js
+++ templates/script.js
@@ -15,3 +15,55 @@ document.addEventListener('DOMContentLoaded', function() {
         navbar.style.display = showNavbar ? 'flex' : 'none';
     });
 });
+
+
+// 새코드
+
+document.addEventListener('DOMContentLoaded', function() {
+    const slider = document.querySelector(".scrollable-section");
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+    let velocity = 0;
+    let animationFrame;
+
+    function animateScrolling() {
+        if (Math.abs(velocity) < 0.5) {
+            cancelAnimationFrame(animationFrame);
+            return;
+        }
+        
+        slider.scrollLeft += velocity;
+        velocity *= 0.95;  // friction
+        animationFrame = requestAnimationFrame(animateScrolling);
+    }
+
+    slider.addEventListener('mousedown', (e) => {
+        isDown = true;
+        startX = e.pageX;
+        scrollLeft = slider.scrollLeft;
+    });
+
+    slider.addEventListener('mouseleave', () => {
+        isDown = false;
+    });
+
+    slider.addEventListener('mouseup', () => {
+        isDown = false;
+        animateScrolling();
+    });
+
+    slider.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX;
+        const walk = x - startX;
+        velocity = -walk * 0.2  ;
+        slider.scrollLeft = scrollLeft - walk;
+    });
+
+    slider.addEventListener('wheel', function(e) {
+        e.preventDefault();
+        slider.scrollLeft += e.deltaY;
+    });
+});
